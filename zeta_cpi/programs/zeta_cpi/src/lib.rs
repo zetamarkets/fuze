b9ab9952778ddbe960d8f5ef9c88615edc97cea3
tfrizza <tfrizza@gmail.com>
2021-11-28 01:45:41 +0800

    margin account creation and initialization working

--- zeta_cpi/programs/zeta_cpi/src/lib.rs
+++ zeta_cpi/programs/zeta_cpi/src/lib.rs
@@ -27,6 +27,43 @@ pub fn sighash(namespace: &str, name: &str) -> [u8; 8] {
 //     ) -> ProgramResult;
 // }
 
+// TODO: Refactor cpi calls into proc_macro
+
+pub fn create_margin_account_cpi<'a,'b, 'c, 'info, T: anchor_lang::Accounts<'info> + anchor_lang::ToAccountMetas + anchor_lang::ToAccountInfos<'info>>(
+    ctx: anchor_lang::CpiContext<'a, 'b, 'c, 'info, T>,
+    nonce: u8,
+) -> anchor_lang::solana_program::entrypoint::ProgramResult {
+    let SIGHASH_GLOBAL_NAMESPACE = "global";
+    let sighash_arr = sighash(&SIGHASH_GLOBAL_NAMESPACE, &"create_margin_account");
+    #[derive(anchor_lang::AnchorSerialize, anchor_lang::AnchorDeserialize)]
+    struct Args {
+        nonce: u8
+    };
+
+    let ix = {
+        let ix = Args {
+            nonce
+        };
+        let mut ix_data = anchor_lang::AnchorSerialize::try_to_vec(&ix)
+            .map_err(|_| anchor_lang::__private::ErrorCode::InstructionDidNotSerialize)?;
+        let mut data = sighash_arr.to_vec();
+        data.append(&mut ix_data);
+        let accounts = ctx.to_account_metas(None);
+        anchor_lang::solana_program::instruction::Instruction {
+            program_id: *ctx.program.key,
+            accounts,
+            data,
+        }
+    };
+    let mut acc_infos = ctx.to_account_infos();
+    acc_infos.push(ctx.program.clone());
+    anchor_lang::solana_program::program::invoke_signed(
+        &ix,
+        &acc_infos,
+        ctx.signer_seeds,
+    )
+}
+
 pub fn initialize_margin_account_cpi<'a,'b, 'c, 'info, T: anchor_lang::Accounts<'info> + anchor_lang::ToAccountMetas + anchor_lang::ToAccountInfos<'info>>(
     ctx: anchor_lang::CpiContext<'a, 'b, 'c, 'info, T>,
     nonce: u8,
@@ -66,36 +103,44 @@ pub fn initialize_margin_account_cpi<'a,'b, 'c, 'info, T: anchor_lang::Accounts<
 pub mod zeta_cpi {
     use super::*;
 
-    // pub fn initialize_margin_account(ctx: Context<InitializeMarginAccountCaller>) -> ProgramResult {
-    //     let cpi_program = ctx.accounts.zeta_program.clone();
-    //     let cpi_accounts = InitializeMarginAccount {
-    //         zeta_group: ctx.accounts.zeta_group.to_account_info(),
-    //         margin_account: ctx.accounts.margin_account.to_account_info(),
-    //         authority: ctx.accounts.authority.clone(),
-    //         system_program: ctx.accounts.system_program.clone(),
-    //     };
-    //     // TODO: seeds = [MARGIN_SEED.as_bytes().as_ref(), zeta_group.key().as_ref(), authority.key.as_ref()]
-    //     let (_pda, nonce)  = Pubkey::find_program_address(&[MARGIN_SEED.as_bytes()], &cpi_program.key.clone());
-    //     let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-    //     // use lowercase
-    //     sighash_global_namespace::initialize_margin_account(cpi_ctx, nonce)?;
-    //     Ok(())
-    // }
+    pub fn create_margin_account(ctx: Context<CreateMarginAccountCaller>) -> ProgramResult {
+        let cpi_program = ctx.accounts.zeta_program.clone();
+        let cpi_accounts = CreateMarginAccount {
+            margin_account: ctx.accounts.margin_account.clone(),
+            authority: ctx.accounts.authority.clone(),
+            system_program: ctx.accounts.system_program.clone(),
+            zeta_program: cpi_program.clone(),
+            zeta_group: ctx.accounts.zeta_group.clone(),
+        };
+        let (_pda, nonce)  = Pubkey::find_program_address(&[MARGIN_SEED.as_bytes(), ctx.accounts.zeta_group.key.as_ref(), ctx.accounts.authority.key.as_ref()], &cpi_program.key.clone());
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        create_margin_account_cpi(cpi_ctx, nonce)
+    }
 
-    pub fn initialize_margin_account(ctx: Context<InitializeMarginAccountCaller>, nonce: u8) -> ProgramResult {
+    pub fn initialize_margin_account(ctx: Context<InitializeMarginAccountCaller>) -> ProgramResult {
         let cpi_program = ctx.accounts.zeta_program.clone();
         let cpi_accounts = InitializeMarginAccount {
-            zeta_group: ctx.accounts.zeta_group.to_account_info(),
-            margin_account: ctx.accounts.margin_account.to_account_info(),
+            zeta_group: ctx.accounts.zeta_group.clone(),
+            margin_account: ctx.accounts.margin_account.clone(),
             authority: ctx.accounts.authority.clone(),
             system_program: ctx.accounts.system_program.clone(),
         };
-        // TODO: seeds = [MARGIN_SEED.as_bytes().as_ref(), zeta_group.key().as_ref(), authority.key.as_ref()]
-        // let (_pda, nonce)  = Pubkey::find_program_address(&[MARGIN_SEED.as_bytes(), ctx.accounts.zeta_group.key().as_ref(), ctx.accounts.authority.key.as_ref()], &cpi_program.key.clone());
+        let (_pda, nonce)  = Pubkey::find_program_address(&[MARGIN_SEED.as_bytes(), ctx.accounts.zeta_group.key.as_ref(), ctx.accounts.authority.key.as_ref()], &cpi_program.key.clone());
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        // initialize_margin_account_cpi(cpi_ctx, nonce)?;
-        Ok(())
+        initialize_margin_account_cpi(cpi_ctx, nonce)
     }
+}
+
+#[derive(Accounts)]
+pub struct CreateMarginAccount<'info> {
+    #[account(mut)]
+    pub margin_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    #[account(address = ID)]
+    pub zeta_program: AccountInfo<'info>,
+    pub zeta_group: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
@@ -105,7 +150,7 @@ pub struct InitializeMarginAccount<'info> {
     pub zeta_group: AccountInfo<'info>,
     #[account(
         mut,
-        seeds = [MARGIN_SEED.as_bytes().as_ref(), zeta_group.key().as_ref(), authority.key.as_ref()],
+        seeds = [MARGIN_SEED.as_bytes().as_ref(), zeta_group.key.as_ref(), authority.key.as_ref()],
         bump = nonce,
     )]
     pub margin_account: AccountInfo<'info>,
@@ -114,41 +159,27 @@ pub struct InitializeMarginAccount<'info> {
     pub system_program: Program<'info, System>,
 }
 
-#[derive(Accounts)]
-#[instruction(nonce: u8)]
+// Dummy caller fns
 
+#[derive(Accounts)]
+pub struct CreateMarginAccountCaller<'info> {
+    #[account(mut)]
+    pub margin_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub zeta_program: AccountInfo<'info>,
+    pub zeta_group: AccountInfo<'info>,
+}
+
+// Note to self: don't do seeds validation in the outer call because that will use the calling programid
+#[derive(Accounts)]
 pub struct InitializeMarginAccountCaller<'info> {
     pub zeta_program: AccountInfo<'info>,
     pub zeta_group: AccountInfo<'info>,
-    #[account(
-        mut,
-        seeds = [MARGIN_SEED.as_bytes().as_ref(), zeta_group.key().as_ref(), authority.key.as_ref()],
-        bump = nonce,
-    )]
+    #[account(mut)]
     pub margin_account: AccountInfo<'info>,
     #[account(mut)]
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
-
-/*
-Error: 101: Fallback functions are not supported
-"As far as I know this means that the sighash of the instruction you're trying to run doesn't match anything in the program. 
-Is it possible you're trying to call a stale version of the program or something?""
-*/
-
-// pub fn initialize_margin_account<'a, 'b, 'c, 'info>(ctx: CpiContext<'a, 'b, 'c, 'info, InitializeMarginAccountCaller<'info>>) -> ProgramResult {
-//     let cpi_program = ctx.accounts.zeta_program.clone();
-//     let (_pda, nonce)  = Pubkey::find_program_address(&[MARGIN_SEED.as_bytes()], &cpi_program.key.clone());
-//     let ix = initialize(ctx.accounts.zeta_group.key, ctx.accounts.margin_account.key, ctx.accounts.authority.key, ctx.accounts.system_program.key, nonce)?;
-//     solana_program::program::invoke_signed(       
-//         &ix,
-//         &[
-//             ctx.accounts.zeta_group.clone(),
-//             ctx.accounts.margin_account.clone(),
-//             ctx.accounts.authority.to_account_info(),
-//             ctx.accounts.system_program.to_account_info(),
-//         ],
-//         ctx.signer_seeds,
-//     )
-// }
--- zeta_cpi/tests/zeta_cpi.ts
+++ zeta_cpi/tests/zeta_cpi.ts
@@ -1,5 +1,5 @@
 import * as anchor from "@project-serum/anchor";
-import { utils, Exchange } from "@zetamarkets/sdk";
+import { utils, Exchange, Network } from "@zetamarkets/sdk";
 
 describe("zeta_cpi", () => {
   // Configure the client to use the local cluster.
@@ -16,31 +16,71 @@ describe("zeta_cpi", () => {
     "So11111111111111111111111111111111111111112"
   );
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const [zetaGroup, _zetaGroupNonce] = await utils.getZetaGroup(
-      zetaProgram,
-      underlyingMint
-    );
-    const [marginAccount, _marginNonce] = await utils.getMarginAccount(
-      zetaProgram,
-      zetaGroup,
-      userKey
-    );
+  let [zetaGroup, _zetaGroupNonce] = [undefined, undefined];
+  let [marginAccount, _marginNonce] = [undefined, undefined];
 
-    console.log(`User: ${userKey}`);
-    console.log(`Zeta group account: ${zetaGroup}`);
-    console.log(`Margin account: ${marginAccount}`);
+  // it("Create margin account via CPI", async () => {
+  //   [zetaGroup, _zetaGroupNonce] = await utils.getZetaGroup(
+  //     zetaProgram,
+  //     underlyingMint
+  //   );
+  //   [marginAccount, _marginNonce] = await utils.getMarginAccount(
+  //     zetaProgram,
+  //     zetaGroup,
+  //     userKey
+  //   );
 
-    const tx = await program.rpc.initializeMarginAccount(_marginNonce, {
-      accounts: {
-        zetaProgram: zetaProgram,
-        zetaGroup: zetaGroup,
-        marginAccount: marginAccount,
-        authority: userKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      },
-    });
-    console.log("Your transaction signature", tx);
-  });
+  //   console.log(`User: ${userKey}`);
+  //   console.log(`Zeta group account: ${zetaGroup}`);
+  //   console.log(`Margin account: ${marginAccount}`);
+
+  //// TODO: remove for debugging
+  // await Exchange.load(
+  //   zetaProgram,
+  //   Network.DEVNET,
+  //   provider.connection,
+  //   utils.defaultCommitment(),
+  //   provider.wallet,
+  //   0
+  // );
+  // const txix = await Exchange.program.instruction.createMarginAccount(
+  //   _marginNonce,
+  //   {
+  //     accounts: {
+  //       marginAccount: marginAccount,
+  //       authority: userKey,
+  //       systemProgram: anchor.web3.SystemProgram.programId,
+  //       zetaProgram: zetaProgram,
+  //       zetaGroup: zetaGroup,
+  //     },
+  //   }
+  // );
+  // console.log(`Create tx: ${txix}`);
+  ////
+
+  // FYI can only create this once
+  // const tx = await program.rpc.createMarginAccount({
+  //   accounts: {
+  //     marginAccount: marginAccount,
+  //     authority: userKey,
+  //     systemProgram: anchor.web3.SystemProgram.programId,
+  //     zetaProgram: zetaProgram,
+  //     zetaGroup: zetaGroup,
+  //   },
+  // });
+  // console.log("Your transaction signature", tx);
+  // });
+
+  // it("Init margin account via CPI", async () => {
+  //   const tx = await program.rpc.initializeMarginAccount({
+  //     accounts: {
+  //       zetaProgram: zetaProgram,
+  //       zetaGroup: zetaGroup,
+  //       marginAccount: marginAccount,
+  //       authority: userKey,
+  //       systemProgram: anchor.web3.SystemProgram.programId,
+  //     },
+  //   });
+  //   console.log("Your transaction signature", tx);
+  // });
 });
