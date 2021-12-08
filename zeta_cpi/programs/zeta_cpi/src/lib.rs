af137b14ded71a28e339dc7e32605335791a6caf
xjcaa <andrewchen67@gmail.com>
2021-12-08 12:02:29 +0800

    First iteration

--- zeta_cpi/programs/zeta_cpi/src/lib.rs
+++ zeta_cpi/programs/zeta_cpi/src/lib.rs
@@ -2,16 +2,16 @@ use anchor_lang::prelude::*;
 use rust_decimal::prelude::*;
 
 pub mod context;
-pub mod zeta_client;
-pub mod zeta_context;
-pub mod zeta_constants;
-pub mod zeta_utils;
-pub mod zeta_account;
 pub mod pyth_client;
+pub mod zeta_account;
+pub mod zeta_client;
+pub mod zeta_constants;
+pub mod zeta_context;
+pub mod zeta_utils;
 use crate::context::*;
-use crate::zeta_utils::*;
 use crate::zeta_account::*;
 use crate::zeta_constants::*;
+use crate::zeta_utils::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -20,58 +20,165 @@ pub mod zeta_cpi {
     use super::*;
 
     pub fn initialize_margin_account(ctx: Context<InitializeMarginAccountCaller>) -> ProgramResult {
-        zeta_client::initialize_margin_account(ctx.accounts.zeta_program.clone(), ctx.accounts.initialize_margin_cpi_accounts.clone())
+        zeta_client::initialize_margin_account(
+            ctx.accounts.zeta_program.clone(),
+            ctx.accounts.initialize_margin_cpi_accounts.clone(),
+        )
     }
 
     pub fn deposit(ctx: Context<DepositCaller>, amount: u64) -> ProgramResult {
-        zeta_client::deposit(ctx.accounts.zeta_program.clone(), ctx.accounts.deposit_cpi_accounts.clone(), amount)
+        zeta_client::deposit(
+            ctx.accounts.zeta_program.clone(),
+            ctx.accounts.deposit_cpi_accounts.clone(),
+            amount,
+        )
     }
 
     pub fn withdraw(ctx: Context<WithdrawCaller>, amount: u64) -> ProgramResult {
-        zeta_client::withdraw(ctx.accounts.zeta_program.clone(), ctx.accounts.withdraw_cpi_accounts.clone(), amount)
+        zeta_client::withdraw(
+            ctx.accounts.zeta_program.clone(),
+            ctx.accounts.withdraw_cpi_accounts.clone(),
+            amount,
+        )
     }
 
     pub fn initialize_open_orders(ctx: Context<InitializeOpenOrdersCaller>) -> ProgramResult {
-        zeta_client::initialize_open_orders(ctx.accounts.zeta_program.clone(), ctx.accounts.initialize_open_orders_cpi_accounts.clone())
+        zeta_client::initialize_open_orders(
+            ctx.accounts.zeta_program.clone(),
+            ctx.accounts.initialize_open_orders_cpi_accounts.clone(),
+        )
     }
 
     pub fn place_order(
-        ctx: Context<PlaceOrderCaller>,         
+        ctx: Context<PlaceOrderCaller>,
         price: u64,
         size: u32,
-        side: Side
+        side: Side,
     ) -> ProgramResult {
-        zeta_client::place_order(ctx.accounts.zeta_program.clone(), ctx.accounts.place_order_cpi_accounts.clone(), price, size, side)
+        zeta_client::place_order(
+            ctx.accounts.zeta_program.clone(),
+            ctx.accounts.place_order_cpi_accounts.clone(),
+            price,
+            size,
+            side,
+        )
     }
 
-    pub fn cancel_order(ctx: Context<CancelOrderCaller>, side: Side, order_id: u128) -> ProgramResult {
-        zeta_client::cancel_order(ctx.accounts.zeta_program.clone(), ctx.accounts.cancel_order_cpi_accounts.clone(), side, order_id)
+    pub fn cancel_order(
+        ctx: Context<CancelOrderCaller>,
+        side: Side,
+        order_id: u128,
+    ) -> ProgramResult {
+        zeta_client::cancel_order(
+            ctx.accounts.zeta_program.clone(),
+            ctx.accounts.cancel_order_cpi_accounts.clone(),
+            side,
+            order_id,
+        )
     }
 
     pub fn read_program_data(ctx: Context<ReadProgramData>) -> ProgramResult {
-        let state = deserialize_account_info::<State>(&ctx.accounts.state).unwrap();
-        msg!("state.num_underlyings: {:?}", state.num_underlyings);
+        let zeta_group =
+            deserialize_account_info_zerocopy::<ZetaGroup>(&ctx.accounts.zeta_group).unwrap();
 
-        let zeta_group = deserialize_account_info_zerocopy::<ZetaGroup>(&ctx.accounts.zeta_group).unwrap();
+        // Get the data for the front expiration.
         let front_expiry_index = zeta_group.front_expiry_index as usize;
-        msg!("Market strike: {:?}", zeta_group.get_strike(front_expiry_index));
-        msg!("Market expiry: {:?}", zeta_group.expiry_series[front_expiry_index].expiry_ts);
-        msg!("Is market expired?: {:?}", zeta_group.expiry_series[front_expiry_index].status().unwrap() == ExpirySeriesStatus::Expired);
-        
+        let expiry_series = zeta_group.expiry_series[front_expiry_index];
+        {
+            // The unix timestamp that the products are tradeable.
+            msg!("Active timestamp {}", expiry_series.active_ts);
+
+            // The unix timestamp that the products expire.
+            msg!("Expiry timestamp {}", expiry_series.expiry_ts);
+
+            let status = zeta_group.expiry_series[front_expiry_index].status()?;
+
+            // If the market is tradeable.
+            msg!("Is market live?: {:?}", status == ExpirySeriesStatus::Live);
+
+            // If the market has expired.
+            msg!(
+                "Is market expired?: {:?}",
+                status == ExpirySeriesStatus::Expired
+            );
+        }
+
+        // Show the data for all products in a given expiry series. Use the front expiry.
+        let products_slice = zeta_group.get_products_slice(front_expiry_index);
+        for i in 0..products_slice.len() {
+            let product = &products_slice[i];
+
+            // The market index of the given product.
+            // This allows for direct indexing into zeta_group.
+            // i.e `product == &zeta_group.products[product_index]`
+            let market_index = get_products_slice_market_index(front_expiry_index, i);
+
+            // Strike has 6 decimals of precision.
+            let strike = product.strike.get_strike()?;
+
+            // The serum market this product trades on.
+            let market = product.market;
+
+            // Call / Put / Future
+            let kind = product.kind;
+
+            msg!(&format!(
+                "Market index = {}, Strike = {}, Market = {}, Kind = {:?}",
+                market_index, strike, market, kind
+            ));
+        }
+
+        // This returns the oracle price as a fixed point integer with 6 decimals of precision
         let oracle_price = get_native_oracle_price(&ctx.accounts.oracle);
         msg!("Oracle price {:?}", oracle_price);
-        
+
+        // Get the mark price and greek data for the first product in the expiry series.
+        // This happens to be the lowest strike call.
+        let market_index = get_products_slice_market_index(front_expiry_index, 0);
+        let greeks_index = get_greeks_index(front_expiry_index, market_index);
+
         let greeks = deserialize_account_info_zerocopy::<Greeks>(&ctx.accounts.greeks).unwrap();
-        msg!("Mark price: {:?}", greeks.mark_prices[front_expiry_index]);
-        msg!("Delta: {:?}", greeks.product_greeks[front_expiry_index].delta);
-        msg!("Vega: {:?}", Decimal::from(greeks.product_greeks[front_expiry_index].vega));
-        msg!("IV: {:?}", Decimal::from(greeks.product_greeks[front_expiry_index].volatility));
-        
-        let margin_account = deserialize_account_info_zerocopy::<MarginAccount>(&ctx.accounts.margin_account).unwrap();
-        msg!("Margin balance: {:?}", margin_account.balance);
-        msg!("Market net position: {:?}", margin_account.positions[front_expiry_index].position);
-        msg!("Market initial margin: {:?}", margin_account.get_initial_margin(&greeks, &zeta_group, oracle_price));
-        msg!("Market maintenance margin: {:?}", margin_account.get_maintenance_margin(&greeks, &zeta_group, oracle_price));
+
+        msg!(&format!(
+            "Market index = {}, Mark price = {}, Delta = {}, Vega = {:?}, IV = {:?}",
+            market_index,
+            greeks.mark_prices[market_index],
+            greeks.product_greeks[greeks_index].delta,
+            Decimal::from(greeks.product_greeks[greeks_index].vega),
+            Decimal::from(greeks.product_greeks[greeks_index].volatility)
+        ));
+
+        let margin_account =
+            deserialize_account_info_zerocopy::<MarginAccount>(&ctx.accounts.margin_account)
+                .unwrap();
+
+        msg!("Margin account balance: {:?}", margin_account.balance);
+
+        // Position details for a given market index.
+        let position = margin_account.positions[market_index].position;
+        let cost_of_trades = margin_account.positions[market_index].cost_of_trades;
+
+        msg!(
+            "MarginAccount position for market index {}: Position={}, Cost of trades={}",
+            market_index,
+            position,
+            cost_of_trades
+        );
+
+        let initial_margin_requirement =
+            margin_account.get_initial_margin(&greeks, &zeta_group, oracle_price);
+        let maintenance_margin_requirement =
+            margin_account.get_maintenance_margin(&greeks, &zeta_group, oracle_price);
+        let total_margin_requirement = initial_margin_requirement
+            .checked_add(maintenance_margin_requirement)
+            .unwrap();
+
+        msg!(
+            "Margin account: Initial: {}, Maintenance: {}, Total: {}",
+            initial_margin_requirement,
+            maintenance_margin_requirement,
+            total_margin_requirement
+        );
 
         Ok(())
     }
@@ -90,5 +197,5 @@ pub enum ErrorCode {
     #[msg("Market not live")]
     MarketNotLive,
     #[msg("Product dirty")]
-    ProductDirty
+    ProductDirty,
 }
--- zeta_cpi/programs/zeta_cpi/src/zeta_utils.rs
+++ zeta_cpi/programs/zeta_cpi/src/zeta_utils.rs
@@ -1,7 +1,7 @@
 use crate::*;
-use std::cell::{RefMut};
-use std::ops::DerefMut;
+use std::cell::RefMut;
 use std::convert::{TryFrom, TryInto};
+use std::ops::DerefMut;
 
 #[macro_export]
 macro_rules! wrap_error {
@@ -12,161 +12,162 @@ macro_rules! wrap_error {
 }
 
 pub fn deserialize_account_info_zerocopy<'a, T: bytemuck::Pod>(
-  account_info: &'a AccountInfo,
+    account_info: &'a AccountInfo,
 ) -> Result<RefMut<'a, T>> {
-  let data = account_info.try_borrow_mut_data()?;
-  Ok(RefMut::map(data, |data| {
-      bytemuck::from_bytes_mut(&mut data.deref_mut()[8..])
-  }))
+    let data = account_info.try_borrow_mut_data()?;
+    Ok(RefMut::map(data, |data| {
+        bytemuck::from_bytes_mut(&mut data.deref_mut()[8..])
+    }))
 }
 
 #[inline(never)]
 pub fn deserialize_account_info<'a, T: AccountSerialize + AccountDeserialize + Owner + Clone>(
-  account_info: &AccountInfo<'a>,
+    account_info: &AccountInfo<'a>,
 ) -> Result<T> {
     let mut data: &[u8] = &account_info.try_borrow_data()?;
     Ok(T::try_deserialize_unchecked(&mut data)?)
 }
 
 pub fn get_otm_amount(spot: u64, strike: u64, product: Kind) -> Result<u64> {
-  match product {
-      Kind::Call => Ok((strike as i128)
-          .checked_sub(spot as i128)
-          .unwrap()
-          .max(0)
-          .try_into()
-          .unwrap()),
-      Kind::Put => Ok((spot as i128)
-          .checked_sub(strike as i128)
-          .unwrap()
-          .max(0)
-          .try_into()
-          .unwrap()),
-      _ => return wrap_error!(Err(ErrorCode::UnsupportedKind.into())),
-  }
+    match product {
+        Kind::Call => Ok((strike as i128)
+            .checked_sub(spot as i128)
+            .unwrap()
+            .max(0)
+            .try_into()
+            .unwrap()),
+        Kind::Put => Ok((spot as i128)
+            .checked_sub(strike as i128)
+            .unwrap()
+            .max(0)
+            .try_into()
+            .unwrap()),
+        _ => return wrap_error!(Err(ErrorCode::UnsupportedKind.into())),
+    }
 }
 
+// TODO refactor and move constants to a file.
 /// Initial margin for single product
 pub fn get_initial_margin_per_lot(
-  spot: u64,
-  strike: u64,
-  mark: u64,
-  product: Kind,
-  side: Side,
+    spot: u64,
+    strike: u64,
+    mark: u64,
+    product: Kind,
+    side: Side,
 ) -> Result<u64> {
-  let initial_margin: u128 = match product {
-      // 15% of Spot
-      Kind::Future => (spot as u128)
-          .checked_mul(1500)
-          .unwrap()
-          .checked_div(10000)
-          .unwrap(),
-      Kind::Call | Kind::Put => {
-          let otm_amount = get_otm_amount(spot, strike, product)?;
-          match side {
-              Side::Bid => {
-                  // min(100% * mark price, 15% of spot)
-                  // Place holder calcs in place for 100% * mark price
-                  (spot as u128)
-                      .checked_mul(1500)
-                      .unwrap()
-                      .checked_div(10000)
-                      .unwrap()
-                      .min(
-                          (mark as u128)
-                              .checked_mul(
-                                  MARK_PRICE_PERCENTAGE.checked_mul(10u128.pow(2)).unwrap(),
-                              )
-                              .unwrap()
-                              .checked_div(10000)
-                              .unwrap(),
-                      )
-              }
-              Side::Ask => {
-                  // max(25% - OTM Amount/spot, 10%)
-                  let percentage_multiplier: u128 = 1000u128.max(
-                      2500u128
-                          .checked_sub(
-                              // Convert OTM/Spot to a percentage
-                              (otm_amount as u128)
-                                  .checked_mul(10000)
-                                  .unwrap()
-                                  .checked_div(spot as u128)
-                                  .unwrap(),
-                          )
-                          .unwrap_or(0),
-                  );
-                  // Scale spot by the percentage multiplier
-                  (spot as u128)
-                      .checked_mul(percentage_multiplier)
-                      .unwrap()
-                      .checked_div(10000)
-                      .unwrap()
-              }
-              Side::Uninitialized => unreachable!(),
-          }
-      }
-      _ => return wrap_error!(Err(ErrorCode::UnsupportedKind.into())),
-  };
-  Ok(u64::try_from(initial_margin).unwrap())
+    let initial_margin: u128 = match product {
+        // 15% of Spot
+        Kind::Future => (spot as u128)
+            .checked_mul(1500)
+            .unwrap()
+            .checked_div(10000)
+            .unwrap(),
+        Kind::Call | Kind::Put => {
+            let otm_amount = get_otm_amount(spot, strike, product)?;
+            match side {
+                Side::Bid => {
+                    // min(100% * mark price, 15% of spot)
+                    // Place holder calcs in place for 100% * mark price
+                    (spot as u128)
+                        .checked_mul(1500)
+                        .unwrap()
+                        .checked_div(10000)
+                        .unwrap()
+                        .min(
+                            (mark as u128)
+                                .checked_mul(
+                                    MARK_PRICE_PERCENTAGE.checked_mul(10u128.pow(2)).unwrap(),
+                                )
+                                .unwrap()
+                                .checked_div(10000)
+                                .unwrap(),
+                        )
+                }
+                Side::Ask => {
+                    // max(25% - OTM Amount/spot, 10%)
+                    let percentage_multiplier: u128 = 1000u128.max(
+                        2500u128
+                            .checked_sub(
+                                // Convert OTM/Spot to a percentage
+                                (otm_amount as u128)
+                                    .checked_mul(10000)
+                                    .unwrap()
+                                    .checked_div(spot as u128)
+                                    .unwrap(),
+                            )
+                            .unwrap_or(0),
+                    );
+                    // Scale spot by the percentage multiplier
+                    (spot as u128)
+                        .checked_mul(percentage_multiplier)
+                        .unwrap()
+                        .checked_div(10000)
+                        .unwrap()
+                }
+                Side::Uninitialized => unreachable!(),
+            }
+        }
+        _ => return wrap_error!(Err(ErrorCode::UnsupportedKind.into())),
+    };
+    Ok(u64::try_from(initial_margin).unwrap())
 }
 
 /// Maintenance margin for single product
 pub fn get_maintenance_margin_per_lot(
-  spot: u64,
-  strike: u64,
-  mark: u64,
-  product: Kind,
-  long: bool,
+    spot: u64,
+    strike: u64,
+    mark: u64,
+    product: Kind,
+    long: bool,
 ) -> Result<u64> {
-  let maintenance_margin: u128 = match product {
-      // 7.5% of Spot
-      Kind::Future => (spot as u128)
-          .checked_mul(750)
-          .unwrap()
-          .checked_div(10000)
-          .unwrap(),
-      Kind::Call | Kind::Put => {
-          let otm_amount = get_otm_amount(spot, strike, product)?;
-          if long {
-              // min(100% * mark price, 7.5% of spot)
-              // Place holder calcs for 100% * mark price
-              (spot as u128)
-                  .checked_mul(750)
-                  .unwrap()
-                  .checked_div(10000)
-                  .unwrap()
-                  .min(
-                      (mark as u128)
-                          .checked_mul(10000)
-                          .unwrap()
-                          .checked_div(10000)
-                          .unwrap(),
-                  )
-          } else {
-              // max((12.5% - OTM Amount/spot)*spot, 5% * spot)
-              let percentage_multiplier: u128 = 500u128.max(
-                  1250u128
-                      .checked_sub(
-                          (otm_amount as u128)
-                              .checked_mul(10000)
-                              .unwrap()
-                              .checked_div(spot as u128)
-                              .unwrap(),
-                      )
-                      .unwrap_or(0),
-              );
+    let maintenance_margin: u128 = match product {
+        // 7.5% of Spot
+        Kind::Future => (spot as u128)
+            .checked_mul(750)
+            .unwrap()
+            .checked_div(10000)
+            .unwrap(),
+        Kind::Call | Kind::Put => {
+            let otm_amount = get_otm_amount(spot, strike, product)?;
+            if long {
+                // min(100% * mark price, 7.5% of spot)
+                // Place holder calcs for 100% * mark price
+                (spot as u128)
+                    .checked_mul(750)
+                    .unwrap()
+                    .checked_div(10000)
+                    .unwrap()
+                    .min(
+                        (mark as u128)
+                            .checked_mul(10000)
+                            .unwrap()
+                            .checked_div(10000)
+                            .unwrap(),
+                    )
+            } else {
+                // max((12.5% - OTM Amount/spot)*spot, 5% * spot)
+                let percentage_multiplier: u128 = 500u128.max(
+                    1250u128
+                        .checked_sub(
+                            (otm_amount as u128)
+                                .checked_mul(10000)
+                                .unwrap()
+                                .checked_div(spot as u128)
+                                .unwrap(),
+                        )
+                        .unwrap_or(0),
+                );
 
-              (spot as u128)
-                  .checked_mul(percentage_multiplier)
-                  .unwrap()
-                  .checked_div(10000)
-                  .unwrap()
-          }
-      }
-      _ => return wrap_error!(Err(ErrorCode::UnsupportedKind.into())),
-  };
-  Ok(u64::try_from(maintenance_margin).unwrap())
+                (spot as u128)
+                    .checked_mul(percentage_multiplier)
+                    .unwrap()
+                    .checked_div(10000)
+                    .unwrap()
+            }
+        }
+        _ => return wrap_error!(Err(ErrorCode::UnsupportedKind.into())),
+    };
+    Ok(u64::try_from(maintenance_margin).unwrap())
 }
 
 /// Returns the native oracle price (6.dp)
@@ -175,12 +176,48 @@ pub fn get_maintenance_margin_per_lot(
 ///
 /// * `oracle` - Oracle account.
 pub fn get_native_oracle_price(oracle: &AccountInfo) -> u64 {
-  let oracle_price = pyth_client::Price::load(&oracle).unwrap();
-  (oracle_price.agg.price as u128)
-      .checked_mul(10u128.pow(PLATFORM_PRECISION.into()))
-      .unwrap()
-      .checked_div(10u128.pow((-oracle_price.expo).try_into().unwrap()))
-      .unwrap()
-      .try_into()
-      .unwrap()
+    let oracle_price = pyth_client::Price::load(&oracle).unwrap();
+    (oracle_price.agg.price as u128)
+        .checked_mul(10u128.pow(PLATFORM_PRECISION.into()))
+        .unwrap()
+        .checked_div(10u128.pow((-oracle_price.expo).try_into().unwrap()))
+        .unwrap()
+        .try_into()
+        .unwrap()
+}
+
+/// Returns the market index given an expiry index and index into the slice.
+///
+/// # Arguments
+///
+/// * `expiry_index` - Expiry series index.
+/// * `product_index` - Index into the products slice. [0..NUM_PRODUCTS_PER_SERIES).
+pub fn get_products_slice_market_index(expiry_index: usize, product_index: usize) -> usize {
+    expiry_index
+        .checked_mul(NUM_PRODUCTS_PER_SERIES)
+        .unwrap()
+        .checked_add(product_index)
+        .unwrap()
+}
+
+/// Returns the greeks index for a given market index.
+///
+/// # Arguments
+///
+/// * `expiry_index` - Expiry series index.
+/// * `market_index` - The market index of the product to get greeks for.
+///
+/// Returns - index into ZetaGroup.product_greeks.
+pub fn get_greeks_index(expiry_index: usize, market_index: usize) -> usize {
+    let slice_product_index = market_index % NUM_PRODUCTS_PER_SERIES;
+    // There is no greeks index for the futures market index which are
+    // in multiples of NUM_PRODUCTS_PER_SERIES - 1.
+    assert!(slice_product_index != NUM_PRODUCTS_PER_SERIES - 1);
+    // The greeks for calls and puts are identical and thus use the same slice position.
+    let slice_position = slice_product_index % NUM_STRIKES;
+    expiry_index
+        .checked_mul(NUM_STRIKES)
+        .unwrap()
+        .checked_add(slice_position)
+        .unwrap()
 }
