0fe6a8684f0d00683a9c450ebfb4f9d58c52da6b
kelvinlau20100 <kelvin@sierra.team>
2021-12-29 14:02:10 +1100

    script to replace pid

---
+++ vault/scripts/replace-pid.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ROOT="$(git rev-parse --show-toplevel)/vault"
+
+echo $ROOT
+
+old_pubkey=$(solana-keygen pubkey ./target/deploy/vault-keypair.json)
+rm $ROOT/target/deploy/vault-keypair.json
+solana-keygen new --outfile $ROOT/target/deploy/vault-keypair.json
+new_pubkey=$(solana-keygen pubkey ./target/deploy/vault-keypair.json)
+
+echo "old_pubkey = $old_pubkey"
+echo "new_pubkey = $new_pubkey"
+
+sed -i "s/$old_pubkey/$new_pubkey/g" $ROOT/Anchor.toml
+sed -i "s/$old_pubkey/$new_pubkey/g" $ROOT/programs/vault/src/lib.rs
+
+cd $ROOT
